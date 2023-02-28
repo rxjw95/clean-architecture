50e1e619b0e7c8c6f2afe374c86290d29d6d46a8
Jangwook Ryu <rxjw95@gmail.com>
2023-03-01 05:17:38 +0900

    feature(exception handler 패키지 스코핑)
    - 각 도메인별로 익셉션 처리가 상이할 수 있기 때문에 AdviceController의 basepackage를 설정

---
+++ src/main/java/com/wafo/jpalecture/order/adapter/in/web/advice/OrderExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.wafo.jpalecture.order.adapter.in.web.advice;
+
+import com.wafo.jpalecture.exeption.IllegalProductCountException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(basePackages = {"com.wafo.jpalecture.order"})
+public class OrderExceptionHandler {
+
+    @ExceptionHandler(IllegalProductCountException.class)
+    public ResponseEntity illegalProductCountExceptionHandler(Exception exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+}
--- src/main/java/com/wafo/jpalecture/product/adapter/in/web/advice/ProductExceptionHandler.java
+++ src/main/java/com/wafo/jpalecture/product/adapter/in/web/advice/ProductExceptionHandler.java
@@ -1,12 +1,12 @@
-package com.wafo.jpalecture.order.adapter.in.web.advice;
+package com.wafo.jpalecture.product.adapter.in.web.advice;
 
-import com.wafo.jpalecture.order.exeption.IllegalProductCountException;
+import com.wafo.jpalecture.exeption.IllegalProductCountException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
-public class WebExceptionHandler {
+@ControllerAdvice(basePackages = {"com.wafo.jpalecture.product"})
+public class ProductExceptionHandler {
 
     @ExceptionHandler(IllegalProductCountException.class)
     public ResponseEntity illegalProductCountExceptionHandler(Exception exception) {
